b3c6fbb2c90169f4dbb5c65450ae51bde69f793e
Elisabeth Henry <liz.henry@ouvaton.org>
2017-01-04 15:29:26 +0100

    Use at least expect instead of unwrap when compiling templates

--- src/templates.rs
+++ src/templates.rs
@@ -7,18 +7,22 @@ pub static IBOOKS: &'static [u8] = include_bytes!("../templates/ibooks.xml");
 pub static CONTAINER: &'static [u8] = include_bytes!("../templates/container.xml");
 
 lazy_static! {
-    pub static ref TOC_NCX: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/toc.ncx")).unwrap();
+    pub static ref TOC_NCX: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/toc.ncx")).expect("error compiling 'toc.ncx' template'");
 }
 
 pub mod v2 {
     lazy_static! {
-        pub static ref CONTENT_OPF: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v2/content.opf")).unwrap();
-        pub static ref NAV_XHTML: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v2/nav.xhtml")).unwrap();
+        pub static ref CONTENT_OPF: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v2/content.opf"))
+            .expect("error compiling 'content.opf' (for EPUB 2.0) template");
+        pub static ref NAV_XHTML: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v2/nav.xhtml"))
+            .expect("error compiling 'nav.xhtml' (for EPUB 2.0) template");
     }
 }
 pub mod v3 {
     lazy_static! {
-        pub static ref CONTENT_OPF: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v3/content.opf")).unwrap();
-        pub static ref NAV_XHTML: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v3/nav.xhtml")).unwrap();
+        pub static ref CONTENT_OPF: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v3/content.opf"))
+            .expect("error compiling 'content.opf' (for EPUB 3.0) template");
+        pub static ref NAV_XHTML: ::mustache::Template = ::mustache::compile_str(include_str!("../templates/v3/nav.xhtml"))
+            .expect("error compiling 'nav.xhtml' (for EPUB 3.0) template");
     }
 }
