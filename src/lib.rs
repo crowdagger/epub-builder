1d087b6abc20336d2370e0293b7b50f893156088
Lise Henry <liz.henry@ouvaton.org>
2023-08-11 01:14:32 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -14,22 +14,15 @@
 //!
 //! ```toml
 //! [dependencies]
-//! epub-builder = "0.6"
+//! epub-builder = "0.7"
 //! ```
 //!
 //! # Example
 //!
 //! ```rust
-//! use epub_builder::EpubBuilder;
-//! use epub_builder::Result;
-//! use epub_builder::ZipLibrary;
-//! use epub_builder::EpubContent;
-//! use epub_builder::ReferenceType;
-//! use epub_builder::TocElement;
-//!
 //! use std::io::Write;
 //!
-//! fn run() -> Result<Vec<u8>> {
+//! fn run() -> epub_builder::Result<Vec<u8>> {
 //!     // Some dummy content to fill our books
 //!     let dummy_content = "Dummy content. This should be valid XHTML if you want a valid EPUB!";
 //!     let dummy_image = "Not really a PNG image";
@@ -38,10 +31,12 @@
 //!     let mut output = Vec::<u8>::new();
 //!
 //!     // Create a new EpubBuilder using the zip library
-//!     EpubBuilder::new(ZipLibrary::new()?)?
+//!     epub_builder::EpubBuilder::new(epub_builder::ZipLibrary::new()?)?
 //!     // Set some metadata
 //!         .metadata("author", "Joan Doe")?
 //!         .metadata("title", "Dummy Book")?
+//!     // Set epub version to 3.0
+//!         .epub_version(epub_builder::EpubVersion::V30)
 //!     // Set the stylesheet (create a "stylesheet.css" file in EPUB that is used by some generated files)
 //!         .stylesheet(dummy_css.as_bytes())?
 //!     // Add a image cover file
@@ -49,27 +44,27 @@
 //!     // Add a resource that is not part of the linear document structure
 //!         .add_resource("some_image.png", dummy_image.as_bytes(), "image/png")?
 //!     // Add a cover page
-//!         .add_content(EpubContent::new("cover.xhtml", dummy_content.as_bytes())
+//!         .add_content(epub_builder::EpubContent::new("cover.xhtml", dummy_content.as_bytes())
 //!                      .title("Cover")
-//!                      .reftype(ReferenceType::Cover))?
+//!                      .reftype(epub_builder::ReferenceType::Cover))?
 //!     // Add a title page
-//!         .add_content(EpubContent::new("title.xhtml", dummy_content.as_bytes())
+//!         .add_content(epub_builder::EpubContent::new("title.xhtml", dummy_content.as_bytes())
 //!                      .title("Title")
-//!                      .reftype(ReferenceType::TitlePage))?
+//!                      .reftype(epub_builder::ReferenceType::TitlePage))?
 //!     // Add a chapter, mark it as beginning of the "real content"
-//!         .add_content(EpubContent::new("chapter_1.xhtml", dummy_content.as_bytes())
+//!         .add_content(epub_builder::EpubContent::new("chapter_1.xhtml", dummy_content.as_bytes())
 //!                      .title("Chapter 1")
-//!                      .reftype(ReferenceType::Text))?
+//!                      .reftype(epub_builder::ReferenceType::Text))?
 //!     // Add a second chapter; this one has more toc information about its internal structure
-//!         .add_content(EpubContent::new("chapter_2.xhtml", dummy_content.as_bytes())
+//!         .add_content(epub_builder::EpubContent::new("chapter_2.xhtml", dummy_content.as_bytes())
 //!                      .title("Chapter 2")
-//!                      .child(TocElement::new("chapter_2.xhtml#1", "Chapter 2, section 1")))?
+//!                      .child(epub_builder::TocElement::new("chapter_2.xhtml#1", "Chapter 2, section 1")))?
 //!     // Add a section. Since its level is set to 2, it will be attached to the previous chapter.
-//!         .add_content(EpubContent::new("section.xhtml", dummy_content.as_bytes())
+//!         .add_content(epub_builder::EpubContent::new("section.xhtml", dummy_content.as_bytes())
 //!                      .title("Chapter 2, section 2")
 //!                      .level(2))?
 //!     // Add a chapter without a title, which will thus not appear in the TOC.
-//!         .add_content(EpubContent::new("notes.xhtml", dummy_content.as_bytes()))?
+//!         .add_content(epub_builder::EpubContent::new("notes.xhtml", dummy_content.as_bytes()))?
 //!     // Generate a toc inside of the document, that will be part of the linear structure.
 //!         .inline_toc()
 //!     // Finally, write the EPUB file to a writer. It could be a `Vec<u8>`, a file,
